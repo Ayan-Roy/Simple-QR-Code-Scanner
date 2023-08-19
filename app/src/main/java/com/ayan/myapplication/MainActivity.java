627a036a635ff9a22eefa892e6aad92eef73224b
Ayan-Roy <ayanroycse@gmail.com>
2023-08-20 00:17:54 +0600

    Phone type QR code generated

--- app/src/main/java/com/ayan/myapplication/MainActivity.java
+++ app/src/main/java/com/ayan/myapplication/MainActivity.java
@@ -3,26 +3,52 @@ package com.ayan.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 
 public class MainActivity extends AppCompatActivity {
     private CodeScanner mCodeScanner;
     private String TAG = "MainActivity";
+    ImageView qrImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+
+        qrImage = findViewById(R.id.qrImage);
+
+
+        Bitmap bitmap;
+        // Initializing the QR Encoder with your value to be encoded, type you required and Dimension
+        QRGEncoder qrgEncoder = new QRGEncoder("01741753671", null, QRGContents.Type.PHONE, 100);
+        qrgEncoder.setColorBlack(Color.WHITE);
+        qrgEncoder.setColorWhite(Color.BLACK);
+        try {
+            // Getting QR-Code as Bitmap
+            bitmap = qrgEncoder.getBitmap();
+            // Setting Bitmap to ImageView
+            qrImage.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            Log.v(TAG, e.toString());
+        }
+
+/*        CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
         mCodeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
@@ -30,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+
+
                         Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_LONG).show();
                     }
                 });
@@ -40,18 +68,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 mCodeScanner.startPreview();
             }
-        });
+        });*/
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mCodeScanner.startPreview();
+       // mCodeScanner.startPreview();
     }
 
     @Override
     protected void onPause() {
-        mCodeScanner.releaseResources();
+       // mCodeScanner.releaseResources();
         super.onPause();
     }
 }
